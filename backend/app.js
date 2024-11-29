aeeedf6ff66f8e1dbc596a117d080dd6edf8e1e7
DangMinh <52200277@student.tdtu.edu.vn>
2024-11-29 23:25:27 +0700

    fix cors origin typo

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(
     cors({
-        origin: 'https://ceasar-pos-app-api.vercel.app',
+        origin: 'https://ceasar-pos-app-client.vercel.app',
         credentials: true,
     }),
 )
