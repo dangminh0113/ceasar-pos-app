2df9e87ff7d67f37e8a42c6cbc1f356f6dc5a877
DangMinh <52200277@student.tdtu.edu.vn>
2024-11-30 10:26:14 +0700

    fix multer

--- backend/src/middlewares/uploadImage/multer.js
+++ backend/src/middlewares/uploadImage/multer.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 const avatarStorage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'tmp/uploads/avatars');
+        cb(null, 'uploads/avatars')
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
